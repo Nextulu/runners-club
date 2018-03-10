9f65f40830813c7b940ade8ab649e5f2f124b5db
Jakub Świętoniowski <50next50@gmail.com>
2018-03-10 23:33:47 +0000

    Ajax post request is now working with adding customer feature

--- public/app.js
+++ public/app.js
@@ -68,7 +68,12 @@ function login() {
 //makeRequest("admin.txt");
 makeRequest("customers.json");
 
+let customersList = document.getElementById("customersList");
+
 function showCustomers() {
+	while (customersList.firstChild) {
+		customersList.removeChild(customersList.firstChild);
+	}
 	for(let i = 0; i < customers.length; i++) {
 		let customer = document.createElement("div");
 		let fName = document.createElement("p");
@@ -84,7 +89,7 @@ function showCustomers() {
 		customer.appendChild(lName);
 		customer.appendChild(balance);
 		customer.className = "customer";
-		document.getElementById("content").appendChild(customer);
+		customersList.appendChild(customer);
 	}
 }
 
@@ -133,8 +138,9 @@ document.addEventListener('click', function (event) {
 let btnAddCust = document.getElementById("pushCustomer");
 btnAddCust.addEventListener("click", pushCustomer);
 
-let cust = {};
+
 function pushCustomer() {
+	let cust = {};
 	cust.ID = addFName.value.substring(0, 3) + addLName.value.substring(0, 3);
 	cust.fName = addFName.value;
 	cust.lName = addLName.value;
@@ -151,41 +157,6 @@ function pushCustomer() {
 	}
 	
 	customers.push(cust);
-	let customersJSON = JSON.stringify(customers);
-	createCookie("customersList", customersJSON, 365);
-	
-	
-	console.log(document.cookie);
+	showCustomers();
 	console.log(customers);
 }
-
-var createCookie = function(name, value, days) {
-    var expires;
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		expires = "; expires=" + date.toGMTString();
-		console.log(expires);
-    }
-    else {
-        expires = "";
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function getCookie(c_name) {
-    if (document.cookie.length > 0) {
-        var c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start != -1) {
-            c_start = c_start + c_name.length + 1;
-            var c_end = document.cookie.indexOf(";", c_start);
-            if (c_end == -1) {
-                c_end = document.cookie.length;
-            }
-            return unescape(document.cookie.substring(c_start, c_end));
-        }
-    }
-    return "";
-}
-
-getCookie("customersList");
--- server.js
+++ server.js
@@ -1,6 +1,32 @@
+"use strict";
+
 const express = require("express")
 const app = express()
+const fs = require("fs")
+const bodyParser = require("body-parser")
 
 app.use(express.static("public"))
-
 app.listen(process.env.PORT || 1337, () => console.log("Server is running!"))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+	let file = JSON.parse(fs.readFileSync('./public/customers.json').toString());
+	let customer = {
+		"ID": req.body.fName.substr(0, 3) + req.body.lName.substr(0, 3),
+		"fName": req.body.fName,
+		"lName": req.body.lName,
+		"balance": req.body.balance,
+		"directDebit": req.body.directDebit ? true : false,
+		"membership": req.body.membership ? true : false
+	}
+	file.push(customer)
+	fs.writeFile('./public/customers.json', JSON.stringify(file, null, 2), function(err) {
+		if (err) throw err;
+		console.log('Data written successfully!');
+		res.end();
+	});
+});
